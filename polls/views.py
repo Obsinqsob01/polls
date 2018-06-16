9c0127f99e7b7396d046d093ebb07cf4442f8d07
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-16 12:29:19 -0500

    Agregada consulta a la db

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world in the polls app")
+    q= Question.objects.get(pk=1)
+
+    return HttpResponse("Hello world in the polls app {0}".format(q.question_text))

e04c10b7299629436a1d83269012e9787a9846b9
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-17 15:52:50 -0500

    Added the vote action

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 # from django.template import loader
 
 from .models import Question
@@ -20,9 +21,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "Estas buscando los resultados de la pregunta %s"
+    question = get_object_or_404(Question, pk=question_id)
 
-    return HttpResponse(response%question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("Estas votando en la pregunta %s"%question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'No has seleccionado una opcion'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))

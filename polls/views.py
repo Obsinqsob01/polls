deff0ad2defad1230bd3932b2cedc7c5dcae85a9
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-17 13:38:13 -0500

    Added more routes and views

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,15 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-    q= Question.objects.get(pk=1)
-
     return HttpResponse("Hello world in the polls app {0}".format(q.question_text))
+
+def detail(request, question_id):
+    return HttpResponse('Estas buscando la pregunta %s'%question_id)
+
+def results(request, question_id):
+    response = "Estas buscando los resultados de la pregunta %s"
+
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s"%question_id)

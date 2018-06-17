43f57d2739d2b4bb8bd5742326989443cf8b7299
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-06-17 16:31:50 -0500

    Config ...

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+
 # from django.template import loader
 
 from .models import Question
